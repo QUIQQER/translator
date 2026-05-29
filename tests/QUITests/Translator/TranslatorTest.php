ea11d52af49f55f60ab56142fe46fbe5a9d7782a
Henning <leutz@pcsg.de>
2026-05-29 08:43:55 +0200

    fix: avoid reflection property deprecation in tests

--- tests/QUITests/Translator/TranslatorTest.php
+++ tests/QUITests/Translator/TranslatorTest.php
@@ -133,19 +133,19 @@ JS
         $oldVersion = (string)$Config->get('locale', 'publishVersion');
         $RefClass = new ReflectionClass(Translator::class);
         $versionProperty = $RefClass->getProperty('localePublishVersion');
-        $versionProperty->setValue(null);
+        $versionProperty->setValue(null, null);
 
         try {
             $version1 = Translator::getLocalePublishVersion();
             $this->assertNotSame('', $version1);
 
-            $versionProperty->setValue(null);
+            $versionProperty->setValue(null, null);
             $version2 = Translator::getLocalePublishVersion();
             $this->assertSame($version1, $version2);
         } finally {
             $Config->set('locale', 'publishVersion', $oldVersion);
             $Config->save();
-            $versionProperty->setValue(null);
+            $versionProperty->setValue(null, null);
         }
     }
 
@@ -162,21 +162,21 @@ JS
         $RefClass = new ReflectionClass(Translator::class);
         $versionProperty = $RefClass->getProperty('localePublishVersion');
         $refreshMethod = $RefClass->getMethod('refreshLocalePublishVersion');
-        $versionProperty->setValue(null);
+        $versionProperty->setValue(null, null);
 
         try {
             $versionBefore = Translator::getLocalePublishVersion();
             usleep(1000);
             $refreshMethod->invoke(null);
 
-            $versionProperty->setValue(null);
+            $versionProperty->setValue(null, null);
             $versionAfter = Translator::getLocalePublishVersion();
 
             $this->assertNotSame($versionBefore, $versionAfter);
         } finally {
             $Config->set('locale', 'publishVersion', $oldVersion);
             $Config->save();
-            $versionProperty->setValue(null);
+            $versionProperty->setValue(null, null);
         }
     }
 }
